fe5efc0adb64f7cb1f0c3e183f051f1d0ee056c6
Thane Fitzgerald <tfitzgerald@rogers.com>
2026-02-04 20:12:14 -0600

    Switch build to Debug mode for easier installation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+	packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            merges += "META-INF/LICENSE*"
+        }
+    }
 }
 
 dependencies {
