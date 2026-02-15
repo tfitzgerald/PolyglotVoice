8cf3da6d12ccd2d87a7906888fb45824c468371a
Thane Fitzgerald <tfitzgerald@rogers.com>
2026-02-15 14:31:42 -0600

    complete rewrite 03.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,6 @@
 // Top-level build file
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.7.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-    }
-}
-
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("com.android.library") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
